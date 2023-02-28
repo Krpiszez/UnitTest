6196824f657aeae3760b7a2fcae06ed4c882ba1e
Omer Isik <isikomer4545@gmail.com>
2023-02-28 22:53:42 +0100

    pwd test

--- src/test/java/day01JUnit/SomeReviews.java
+++ src/test/java/day01JUnit/SomeReviews.java
@@ -17,6 +17,21 @@ public class SomeReviews {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testSomeManipulations(){
+        String pwd = "A123&xzca";
+        boolean flag = false;
+        if (pwd.replaceAll("\\p{Punct}", "").length() == pwd.length()){
+            flag = false;
+        } else if (pwd.charAt(0)>='A' && pwd.charAt(0)<='Z') {
+            flag = true;
+        } else if (pwd.charAt(pwd.length()-1)>='a' && pwd.charAt(pwd.length()-1)<='z') {
+            flag = true;
+        }
+
+        assertTrue(flag);
+    }
+
     String getSubstring(String str, int from){
         return str.substring(from);
     }
